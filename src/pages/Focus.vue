dfb932c7b779076e93cf00640ce90840588cf551
Britta Rodenbeck <rodenbeck.britta@gmail.com>
2018-04-20 10:14:04 -0400

    styling structure for focus page

--- src/pages/Focus.vue
+++ src/pages/Focus.vue
@@ -8,6 +8,104 @@
           Ullamco laboris nisi ut aliquip ex ea commodo consequat."
       >
       </hero-section>
+      <div class="focus">
+        <div class="box label">
+          <img src="https://fillmurray.com/100/100" alt="">
+          <p>Automotive Tech</p>
+        </div>
+        <div class="box content">
+          <h2>Headline</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus labore
+            quia cum inventore pariatur facere dolorum officia error, quis laudantium
+            soluta architecto quas earum laboriosam rem voluptatibus mollitia maiores
+            deleniti!
+          </p>
+        </div>
+        <div class="box image">
+          <img src="https://fillmurray.com/800/800" alt="">
+        </div>
+        <div class="box label">
+          <img src="https://fillmurray.com/100/100" alt="">
+          <p>B2B, B2C, &amp; H2H Platforms</p>
+        </div>
+        <div class="box image">
+          <img src="https://fillmurray.com/800/800" alt="">
+        </div>
+        <div class="box content">
+          <h2>Headline</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus labore
+            quia cum inventore pariatur facere dolorum officia error, quis laudantium
+            soluta architecto quas earum laboriosam rem voluptatibus mollitia maiores
+            deleniti!
+          </p>
+        </div>
+        <div class="box label">
+          <img src="https://fillmurray.com/100/100" alt="">
+          <p>FinTech</p>
+        </div>
+        <div class="box content">
+          <h2>Headline</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus labore
+            quia cum inventore pariatur facere dolorum officia error, quis laudantium
+            soluta architecto quas earum laboriosam rem voluptatibus mollitia maiores
+            deleniti!
+          </p>
+        </div>
+        <div class="box image">
+          <img src="https://fillmurray.com/800/800" alt="">
+        </div>
+        <div class="box label">
+          <img src="https://fillmurray.com/100/100" alt="">
+          <p>Wearable Tech</p>
+        </div>
+        <div class="box image">
+          <img src="https://fillmurray.com/800/800" alt="">
+        </div>
+        <div class="box content">
+          <h2>Headline</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus labore
+            quia cum inventore pariatur facere dolorum officia error, quis laudantium
+            soluta architecto quas earum laboriosam rem voluptatibus mollitia maiores
+            deleniti!
+          </p>
+        </div>
+        <div class="box label">
+          <img src="https://fillmurray.com/100/100" alt="">
+          <p>Technology in Motion</p>
+        </div>
+        <div class="box content">
+          <h2>Headline</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus labore
+            quia cum inventore pariatur facere dolorum officia error, quis laudantium
+            soluta architecto quas earum laboriosam rem voluptatibus mollitia maiores
+            deleniti!
+          </p>
+        </div>
+        <div class="box image">
+          <img src="https://fillmurray.com/800/800" alt="">
+        </div>
+        <div class="box label">
+          <img src="https://fillmurray.com/100/100" alt="">
+          <p>Medical &amp; Health</p>
+        </div>
+        <div class="box image">
+          <img src="https://fillmurray.com/800/800" alt="">
+        </div>
+        <div class="box content">
+          <h2>Headline</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus labore
+            quia cum inventore pariatur facere dolorum officia error, quis laudantium
+            soluta architecto quas earum laboriosam rem voluptatibus mollitia maiores
+            deleniti!
+          </p>
+        </div>
+      </div>
     </template>
   </base-page>
 </template>
@@ -27,3 +125,43 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+@import "../assets/styles/variables";
+.focus {
+  @include grid-boxes(1, 18, 100vw, 100vw, 0);
+  h2 {
+    @include title-font;
+  }
+  .box:not(.image) {
+    padding: 25px;
+    display: flex;
+    flex-direction: column;
+    &.content {
+      justify-content: flex-end;
+    }
+    &.label {
+      justify-content: center;
+      align-items: center;
+      background: gray;
+      p {
+        font-size: 36px;
+        color: white;
+        font-weight: 100;
+        text-align: center;
+      }
+    }
+  }
+  .box.image {
+    img {
+      min-height: 100%;
+      min-width: 100%;
+      object-fit: cover;
+    }
+  }
+  @media only screen and (min-width: $medium) {
+    @include grid-boxes(3, 6, (100vw / 3), (100vw / 3), 0);
+  }
+}
+</style>
+
