b442a56040c58e7ec267eabe9003ab3237c9c09c
Britta Rodenbeck <rodenbeck.britta@gmail.com>
2018-04-25 08:11:09 -0400

    adding in mailchimp form for newsletter

--- src/components/NewsletterForm.vue
+++ src/components/NewsletterForm.vue
@@ -1,20 +1,52 @@
 <template>
 <div class="form-container">
-  <form @submit.prevent="submitForm" id="news">
-    <div style="position: absolute; left: -5000px;" aria-hidden="true">
-      <input type="text" name="" v-model="hash" tabindex="-1" value="">
-    </div>
-    <input v-model="email" type="email" name="EMAIL" id="news_email" placeholder="Email address">
-    <button type="submit" class="button inverse">Submit</button>
-  </form>
-  <p v-if="showSuccess">Your message was sent successfully</p>
-  <p v-if="showError">There was an error, please try again later.</p>
+  <!-- Begin MailChimp Signup Form -->
+  <div id="mc_embed_signup">
+    <form
+      action="https://citysideventures.us18.list-manage.com/subscribe/post?u=03ee7dad3e5d26976b6501b49&amp;id=567e4477a2"
+      method="post"
+      id="mc-embedded-subscribe-form"
+      name="mc-embedded-subscribe-form"
+      class="validate"
+      target="_blank"
+      novalidate
+    >
+      <div id="mc_embed_signup_scroll">
+        <div class="mc-field-group">
+          <input
+            type="email"
+            value=""
+            name="EMAIL"
+            class="required email"
+            placeholder="Email address"
+            id="mce-EMAIL"
+          >
+        </div>
+        <div id="mce-responses" class="clear">
+          <div class="response" id="mce-error-response" style="display:none"></div>
+          <div class="response" id="mce-success-response" style="display:none"></div>
+        </div>
+        <div style="position: absolute; left: -5000px;" aria-hidden="true">
+          <input type="text" name="b_03ee7dad3e5d26976b6501b49_567e4477a2" tabindex="-1" value="">
+        </div>
+        <div class="clear">
+          <input
+            type="submit"
+            value="Submit"
+            name="subscribe"
+            id="mc-embedded-subscribe"
+            class="button"
+          >
+        </div>
+      </div>
+    </form>
+  </div>
+  <!--End mc_embed_signup-->
 </div>
 </template>
 
 <script>
 import axios from 'axios';
-import formurlencoded from 'form-urlencoded';
 
 export default {
   name: 'NewsletterForm',
@@ -29,7 +61,7 @@ export default {
   methods: {
     submitForm(data) {
       const config = { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } };
-      axios.post('', formurlencoded(data), config)
+      axios.post('', data, config)
         .then(this.showSuccess = true)
         .catch(this.showError = true);
     },
