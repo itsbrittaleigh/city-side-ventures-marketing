7c46128b3e01491ba2e0da812f5b0d203896bcbf
Britta Rodenbeck <rodenbeck.britta@gmail.com>
2018-05-21 15:28:09 -0400

    adding quote placeholders and alternating colors for team page

--- src/pages/Team.vue
+++ src/pages/Team.vue
@@ -10,18 +10,30 @@
         name="team"
       >
       </hero-section>
-      <div class="team">
+      <div class="team" id="team">
         <div
           v-for="(member, index) in team"
           :key="index"
-          class="member"
+          class="member box"
         >
           <img :src="member.photo" :alt="member.name">
-          <div :class="`label label-${member.color}`">
+          <div class="label">
             <p class="name">{{ member.name }}</p>
             <p><em>{{ member.title }}</em></p>
           </div>
         </div>
+      </div>
+      <div id="quote1" class="box">
+        <p>I am a super interesting quote about stuff.</p>
+        <p class="title">Some guy</p>
+      </div>
+      <div id="quote2" class="box">
+        <p>I am a super interesting quote about stuff.</p>
+        <p class="title">Some guy</p>
+      </div>
+      <div id="quote3" class="box">
+        <p>I am a super interesting quote about stuff.</p>
+        <p class="title">Some guy</p>
       </div>
     </template>
   </base-page>
@@ -46,6 +58,45 @@ export default {
       'team',
     ]),
   },
+  methods: {
+    isEvenRowedOnDesktop(index) {
+      return (((Math.ceil(index / 4) * 4) % 8) === 0);
+    },
+    updateColors() {
+      const boxes = document.getElementsByClassName('box');
+      for (let i = 0; i < boxes.length; i += 1) {
+        if (this.isEvenRowedOnDesktop(i + 1)) {
+          boxes.item(i).classList.add('bkg-offset');
+        }
+      }
+    },
+  },
+  mounted() {
+    const teamContainer = document.getElementById('team');
+    const members = document.getElementsByClassName('member');
+    const quote1 = document.getElementById('quote1');
+    const quote2 = document.getElementById('quote2');
+    const quote3 = document.getElementById('quote3');
+
+    if (this.team.length % 4 === 1) {
+      if (this.team.length <= 10) {
+        teamContainer.classList += ' restructured';
+      } else {
+        teamContainer.insertBefore(quote1, members[members.length - 2]);
+        teamContainer.insertBefore(quote3, members[10]);
+        teamContainer.insertBefore(quote2, members[6]);
+        this.updateColors();
+      }
+    } else if (this.team.length % 4 === 2) {
+      teamContainer.insertBefore(quote1, members[members.length - 2]);
+      teamContainer.insertBefore(quote2, members[6]);
+      this.updateColors();
+    } else if (this.team.length % 4 === 3) {
+      teamContainer.insertBefore(quote1, members[members.length - 2]);
+      this.updateColors();
+    }
+    this.updateColors();
+  },
 };
 </script>
 
@@ -53,27 +104,40 @@ export default {
 @import "../assets/styles/variables";
 .team {
   @include grid-boxes(2, auto, 1fr, 1fr, 0);
+  .box {
+    &:nth-child(even) {
+      .label {
+        background: $danube;
+      }
+    }
+    &:nth-child(odd) {
+      .label {
+        background: $goldendream;
+      }
+    }
+    &.bkg-offset {
+      &:nth-child(even) {
+        .label {
+          background: $goldendream;
+        }
+      }
+      &:nth-child(odd) {
+        .label {
+          background: $danube;
+        }
+      }
+    }
+  }
   .member {
     @include grid-boxes(1, 2, 1fr, auto, 0);
     img {
       width: 100%;
       height: 200px;
       object-fit: cover;
+      filter: grayscale(1.0);
     }
     .label {
       padding: 40px 20px 20px;
-      &.label-yellow {
-        background: $goldendream;
-      }
-      &.label-dark-gray {
-        background: $cod;
-      }
-      &.label-blue {
-        background: $danube;
-      }
-      &.label-light-gray {
-        background: $gray;
-      }
     }
     p {
       font-size: 12px;
@@ -93,6 +157,34 @@ export default {
   }
   @media only screen and (min-width: $large) {
     @include grid-boxes(4, auto, 1fr, 1fr, 0);
+  }
+}
+#quote1,
+#quote2,
+#quote3 {
+  display: none;
+  visibility: hidden;
+}
+@media only screen and (min-width: $medium) {
+  #team {
+    #quote1,
+    #quote2,
+    #quote3 {
+      display: flex;
+      flex-direction: column;
+      visibility: visible;
+      background: $nightshadz;
+      padding: 20px 50px;
+      justify-content: center;
+      p {
+        color: $white;
+        font-size: 22px;
+        font-weight: 100;
+        &.title {
+          @include title-font;
+        }
+      }
+    }
   }
 }
 </style>
