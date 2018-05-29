dcc7893ca0e6d7ffdff69b2feb906a11d7708d11
Britta Rodenbeck <rodenbeck.britta@gmail.com>
2018-05-29 17:15:05 -0400

    adding in new assets

--- _src/js/main.js
+++ _src/js/main.js
@@ -12,6 +12,7 @@ Vue.component('team-grid', require('./components/TeamGrid.vue').default);
 Vue.component('contact-form', require('./components/ContactForm.vue').default);
 Vue.component('newsletter-form', require('./components/NewsletterForm.vue').default);
 Vue.component('social-sharing', require('./components/SocialSharing.vue').default);
+Vue.component('copy-to-clipboard', require('./components/CopyToClipboard.vue').default);
 
 /* eslint-disable no-new */
 new Vue({
