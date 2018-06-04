c5d3d080c44547ebc9934a6f6ba547c3c6462b0e
Britta Rodenbeck <rodenbeck.britta@gmail.com>
2018-06-04 15:54:47 -0400

    adding front end validation to contact form

--- _src/js/components/ContactForm.vue
+++ _src/js/components/ContactForm.vue
@@ -1,27 +1,31 @@
 <template>
-  <form @submit.prevent="submitForm" id="contact" class="hidden contact-form">
+  <form @submit.prevent="checkForm" id="contact" class="hidden contact-form">
     <div class="fields">
       <div class="field hidden">
         <label for="name">Your Name</label>
         <input type="text" id="name" name="name" v-model="name">
+        <p class="error" v-if="errors.name">** {{ errors.name }}</p>
       </div>
       <div class="field hidden">
         <label for="company">Company Name</label>
         <input type="text" id="company" name="company" v-model="company">
+        <p class="error" v-if="errors.company">** {{ errors.company }}</p>
       </div>
       <div class="field hidden hidden">
         <label for="phone">Phone Number</label>
         <input type="tel" id="phone" name="phone" v-model="phone">
+        <p class="error" v-if="errors.phone">** {{ errors.phone }}</p>
       </div>
       <div class="field hidden">
         <label for="email">Email Address</label>
         <input type="email" id="email" name="email" v-model="email">
+        <p class="error" v-if="errors.email">** {{ errors.email }}</p>
       </div>
       <div class="field hidden">
         <label for="message">Message</label>
         <textarea name="message" id="message" rows="10" v-model="message"></textarea>
+        <p class="error" v-if="errors.message">** {{ errors.message }}</p>
       </div>
-      <p v-if="showSuccess">Your message was sent successfully</p>
       <button type="submit" class="button button-black hidden">Submit</button>
     </div>
   </form>
@@ -41,7 +45,7 @@ export default {
         company: '',
         message: '',
       },
-      showSuccess: false,
+      errors: {},
     };
   },
   methods: {
@@ -53,7 +57,35 @@ export default {
           JSON.stringify(this.contact),
           config,
         )
-        .then((this.showSuccess = true));
+        .then(window.location.href = '/contact/thank-you');
+    },
+    checkForm(e) {
+      this.errors = [];
+      if(!this.contact.name) this.errors['name'] = 'Your name is required';
+      if(!this.contact.company) this.errors['company'] = 'Company name is required';
+      if(!this.contact.email) {
+        this.errors['email'] = 'Email address is required';
+      } else if(!this.validEmail(this.email)) {
+        this.errors['email'] = 'Please use a valid email address';
+      }
+      if(!this.contact.phone) this.errors['phone'] = 'Phone number is required';
+      if(!this.contact.message) {
+        this.errors['message'] = 'A message is required';
+      }
+      if(this.isEmpty(this.errors)) this.submitForm();
+      e.preventDefault();
+    },
+    validEmail(email) {
+      let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(String(email).toLowerCase());
+    },
+    isEmpty(obj) {
+      for (let key in obj) {
+        if(obj.hasOwnProperty(key)) {
+          return false;
+        }
+      }
+      return true;
     },
   },
 };
