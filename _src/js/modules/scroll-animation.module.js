066b47f07eb11ccf0d1f0156f8d3cba52dce5a13
Britta Rodenbeck <rodenbeck.britta@gmail.com>
2018-06-18 15:21:11 -0400

    making a garbage fix that half works for scroll position and element visibility when loading below the top of the page

--- _src/js/modules/scroll-animation.module.js
+++ _src/js/modules/scroll-animation.module.js
@@ -1,15 +1,20 @@
 let ScrollAnimationModule = (() => {
   const elems = document.getElementsByClassName('hidden');
   const windowHeight = window.innerHeight;
+  toggleVisibilityClass(elems, windowHeight);
 
   window.addEventListener('scroll', () => {
-    for(let i = 0; i < elems.length; i++) {
-      const posFromTop = elems[i].getBoundingClientRect().top;
-      if(posFromTop - windowHeight <= 0) {
-        elems[i].className = elems[i].className.replace('hidden', 'visible');
-      }
-    }
+    toggleVisibilityClass(elems, windowHeight);
   });
 })();
+
+function toggleVisibilityClass(elems, windowHeight) {
+  for(let i = 0; i < elems.length; i++) {
+    const posFromTop = elems[i].getBoundingClientRect().top;
+    if(posFromTop - windowHeight <= 0) {
+      elems[i].className = elems[i].className.replace('hidden', 'visible');
+    }
+  }
+}
 
 export default ScrollAnimationModule;
