52f355c3b040706096b5bbb6bc2d80f9b2fed65b
Britta Rodenbeck <rodenbeck.britta@gmail.com>
2018-09-14 10:36:09 -0400

    setting up services page in CMS

--- pages/services.vue
+++ pages/services.vue
@@ -1,85 +1,47 @@
 <template>
   <div class="page services">
     <hero-section
-      heading="Our Services"
-      description="From startup management, technology development, and more, CitySide Ventures can provide your business with more than just investment."
+      :heading="header.headline"
+      :description="header.description"
+      :color="header.color"
+      :image="header.image"
+      :lead="header.subhead"
       name="services"
     >
     </hero-section>
-
     <section class="focus-grid">
-      <div class="box label box-dark-gray hidden" v-in-viewport.once>
-        <span class="number">1</span>
-        <p>Early Stage Capital</p>
-      </div>
-      <div class="box content content-dark-gray">
-        <h2 class="hidden" v-in-viewport.once>Early Stage Capital</h2>
-        <p class="hidden" v-in-viewport.once>From pre-seed capital through growth capital, our various funds under management offer qualified tech based startup companies funding from $150K and up to $5M per deal.</p>
-        <nuxt-link to="/contact" class="hidden button button-black" v-in-viewport.once>Request a meeting</nuxt-link>
-      </div>
-      <div class="box image early-stage hidden" v-in-viewport.once>
-        <div class="overlay"></div>
-      </div>
-
-      <div class="box label box-blue hidden" v-in-viewport.once>
-        <span class="number">2</span>
-        <p>IP &amp; Technology Licensing</p>
-      </div>
-      <div class="box content content-blue">
-        <h2 class="hidden" v-in-viewport.once>IP &amp; Technology Licensing</h2>
-        <p class="hidden" v-in-viewport.once>Our exclusive network of corporate ventures provides an offering of complete technology & IP licensing services from perform a to close.</p>
-        <nuxt-link to="/contact" class="hidden button button-blue" v-in-viewport.once>Request a meeting</nuxt-link>
-      </div>
-      <div class="box image tech-licensing hidden" v-in-viewport.once>
-        <div class="overlay"></div>
-      </div>
-
-      <div class="box label box-red hidden" v-in-viewport.once>
-          <span class="number">3</span>
-          <p>Syndication and Fund Management</p>
+      <div
+        v-for="(service, index) in services"
+        :key="index"
+        class="inner-box"
+      >
+        <div class="box label hidden" v-in-viewport.once>
+          <span class="number">{{ index + 1 }}</span>
+          <p>{{ service.title }}</p>
         </div>
-        <div class="box content content-red">
-          <h2 class="hidden" v-in-viewport.once>Syndication and Fund Management</h2>
-          <p class="hidden" v-in-viewport.once>Using our exclusive and innovative tech platform, we provide syndication and fund management services to investors and investor groups. The services include fund formation, real time deal flow, due diligence, and portfolio management.</p>
-          <nuxt-link to="/contact" class="hidden button button-red" v-in-viewport.once>Request a meeting</nuxt-link>
+        <div class="box content">
+          <h2 class="hidden" v-in-viewport.once>{{ service.title }}</h2>
+          <p class="hidden" v-in-viewport.once>{{ service.description }}</p>
+          <nuxt-link to="/contact" class="hidden button" v-in-viewport.once>Request a meeting</nuxt-link>
         </div>
-        <div class="box image syndication hidden" v-in-viewport.once>
+        <div class="box image hidden" v-in-viewport.once>
+          <img :src="service.image" alt="">
           <div class="overlay"></div>
         </div>
-
-        <div class="box label box-yellow hidden" v-in-viewport.once>
-          <span class="number">4</span>
-          <p>Venture Studio and Tech Lab</p>
-        </div>
-        <div class="box content content-yellow">
-          <h2 class="hidden" v-in-viewport.once>Venture Studio and Tech Lab</h2>
-          <p class="hidden" v-in-viewport.once>We provide our portfolio of companies full access to our own multi-discipline venture studio. The services range from branding to full stack mobile, web, and software development. It is second to none.</p>
-          <nuxt-link to="/contact" class="hidden button button-yellow" v-in-viewport.once>Request a meeting</nuxt-link>
-        </div>
-        <div class="box image venture hidden" v-in-viewport.once>
-          <div class="overlay"></div>
-        </div>
-
-        <div class="box label box-gray hidden" v-in-viewport.once>
-          <span class="number">5</span>
-          <p>Interim Startup Management</p>
-        </div>
-        <div class="box content content-gray">
-          <h2 class="hidden" v-in-viewport.once>Interim Startup Management</h2>
-          <p class="hidden" v-in-viewport.once>Gain access to our readily available, exclusive, and highly qualified network of members, business advisors, and experienced business professionals to fill interim position on the management team of your company.</p>
-          <nuxt-link to="/contact" class="hidden button button-gray" v-in-viewport.once>Request a meeting</nuxt-link>
-        </div>
-        <div class="box image startup hidden" v-in-viewport.once>
-          <div class="overlay"></div>
-        </div>
+      </div>
     </section>
   </div>
 </template>
 
 <script>
 import HeroSection from '~/components/Hero.vue';
+import VueMarkdown from 'vue-markdown';
 
 export default {
+  async asyncData({ params }) {
+    const pageData = await import('~/content/pages/services.json');
+    return pageData;
+  },
   data() {
     return {};
   },
