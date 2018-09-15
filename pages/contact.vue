4884d68f7747fb7d223bfdcf6a3bc9639bdca8a3
Britta Rodenbeck <rodenbeck.britta@gmail.com>
2018-09-15 13:26:48 -0400

    setting up contact page in CMS

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,41 +1,41 @@
 <template>
   <div class="page contact">
-    <hero-section
-      heading="Contact Us"
-      description="Our team of experienced, energetic and passionate serial entrepreneurs,
-        know how to execute and do it well. We will go where no investor has gone
-        before. Let’s get in touch."
-      name="contact"
-      color="black"
-    >
-    </hero-section>
-
+    <hero-section :header="header"></hero-section>
     <section class="contact-methods">
       <div class="box box-red hidden" v-in-viewport.once>
-        <img src="~/assets/images/icons/location-pin-white.svg" alt="location pin icon">
-        <h3>Headquarters</h3>
+        <img :src="officeIcon" alt="">
+        <h3>{{ hqHeadline }}</h3>
         <address>
           37000 Grand River Avenue<br>
           Suite #310<br>
           Farmington Hills, MI 48335
         </address>
-        <h3>Offices Worldwide</h3>
+        <h3>{{ officesHeadline }}</h3>
         <p>
-          Detroit | Palo Alto<br>
-          Chicago | Tel Aviv
+          <span
+            v-for="(office, index) in offices"
+            :key="index"
+          >
+            {{ office.location }}
+            <span v-if="index + 1 < offices.length">|</span>
+          </span>
         </p>
       </div>
       <div class="box box-light-gray hidden" v-in-viewport.once>
-        <img src="~/assets/images/icons/phone-white.svg" alt="cell phone icon">
-        <h3>Phone</h3>
-        <a href="tel:+12482367005">(248) 236-7005</a>
-        <h3>Email</h3>
-        <a href="mailto:contact@citysideventures.com">contact@citysideventures.com</a>
+        <img :src="contactIcon" alt="">
+        <div
+          v-for="(method, index) in methods"
+          :key="index"
+        >
+          <h3>{{ method.title }}</h3>
+          <a v-if="method.link" :href="method.link">{{ method.text }}</a>
+          <p v-else>{{ method.text }}</p>
+        </div>
       </div>
       <div class="box box-blue hidden" v-in-viewport.once>
-        <img src="~/assets/images/icons/letter-white.svg" alt="email icon" class="news">
-        <h3>Newsletter</h3>
-        <p>Sign up to receive updates about City Side Ventures</p>
+        <img :src="newsletterIcon" alt="">
+        <h3>{{ newsletterHeadline }}</h3>
+        <p>{{ newsletterDescription }}</p>
         <newsletter-form></newsletter-form>
       </div>
     </section>
@@ -43,7 +43,7 @@
     <div class="bkg-yellow">
       <img src="~/assets/images/home/map-no-dots.svg" alt="">
       <div class="container padded-section">
-        <h2 class="hidden" v-in-viewport.once>We would like to hear from you</h2>
+        <h2 class="hidden" v-in-viewport.once>{{ formHeadline }}</h2>
         <contact-form></contact-form>
       </div>
     </div>
@@ -56,6 +56,10 @@ import HeroSection from '~/components/Hero';
 import NewsletterForm from '~/components/NewsletterForm';
 
 export default {
+  async asyncData({ params}) {
+    const pageData = await import('~/content/pages/contact.json');
+    return pageData;
+  },
   data() {
     return {};
   },
